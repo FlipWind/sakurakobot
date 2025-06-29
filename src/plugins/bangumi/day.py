23da4c5c768333ed10499f5044a859cadb795c6a
flipwind <flipwind.t@gmail.com>
2025-06-29 22:15:47 +0800

    style: add compact support to bangumi day

--- src/plugins/bangumi/day.py
+++ src/plugins/bangumi/day.py
@@ -1,7 +1,13 @@
 from ..utils import *
 from ._request import *
 
-bangumi_today = on_alconna(Alconna("#bangumiday", Args["day?", int, datetime.datetime.today().weekday() + 1]))
+bangumi_today = on_alconna(
+    Alconna(
+        "#bangumiday",
+        Args["day?", int, datetime.datetime.today().weekday() + 1],
+        meta=CommandMeta(compact=True),
+    )
+)
 
 
 @bangumi_today.handle()
@@ -13,6 +19,8 @@ async def _(
         await bangumi_today.send(f"Handle [#bangumitoday] with day [{day.result}]")
 
     res = await get_day_bangumi(day.result)
-    append_list: List[Any] = [f"已知 周{' 一二三四五六日'[day.result]} 番剧如下喵！"] + res
+    append_list: List[Any] = [
+        f"已知 周{' 一二三四五六日'[day.result]} 番剧如下喵！"
+    ] + res
 
     await send_node_messages(event, append_list)
