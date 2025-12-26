209363482e1a4aaa2084ff8b3dd4d909f417815a
flipwind <flipwind.t@gmail.com>
2025-12-26 20:13:08 +0800

    perf: change models name

--- src/plugins/what_happened/main.py
+++ src/plugins/what_happened/main.py
@@ -42,8 +42,8 @@ async def _(
     )
 
     all_models = [
-        "gemini-3-pro",
-        "gemini-3-flash",
+        "gemini-3-pro-preview",
+        "gemini-3-flash-preview",
         "gemini-2.5-flash"
     ]
 
