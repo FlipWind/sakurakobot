88f079bdbcf400a3cadbb374c847a9e50efb2de4
flipwind <flipwind.t@gmail.com>
2026-06-11 21:59:33 +0800

    feat: link quote and storage

--- src/plugins/quote/main.py
+++ src/plugins/quote/main.py
@@ -1,5 +1,6 @@
 from ..utils import *
 from .render import rend_quote_message, rend_quote_messages, QuoteMessage
+import uuid
 
 quote = on_alconna(
     Alconna("#qt", Args["nums?", int, 1], meta=CommandMeta(compact=True))
@@ -73,3 +74,88 @@ async def _(
     encoded_image = base64.b64encode(image_data_bytes).decode("utf-8")
 
     await quote.finish(Message(MessageSegment.image(f"base64://{encoded_image}")))
+    
+qta = on_alconna(
+    Alconna("#qta", Args["nums?", int, 1], meta=CommandMeta(compact=True))
+)
+
+@qta.handle()
+async def _(
+    bot: Bot, event: GroupMessageEvent, nums: Match[int] = AlconnaMatch("nums")
+):
+    if not event.reply:
+        await qta.finish("看起来你还没有指定消息喵。")
+
+    if COMMAND_OUTPUT:
+        await qta.send(
+            f"Handle [#qt] with Message {event.reply.message_id}, nums [{nums.result}]"
+        )
+
+    r = await bot.call_api(
+        "get_group_msg_history",
+        group_id=event.group_id,
+        message_seq=event.reply.message_id,
+        count=nums.result,
+    )
+    
+    messages = r.get("messages", [])
+    quote_messages = []
+    for message in messages:
+        profile = await bot.get_group_member_info(
+            group_id=event.group_id, user_id=message["user_id"]
+        )
+
+        rank_type = QuoteMessage.RankType.NORMAL
+        if profile["role"] == "owner":
+            rank_type = QuoteMessage.RankType.OWNER
+        elif profile["role"] == "admin":
+            rank_type = QuoteMessage.RankType.ADMIN
+        else:
+            rank_type = QuoteMessage.RankType.NORMAL
+
+        header = profile["title"]
+        if rank_type == QuoteMessage.RankType.NORMAL and header:
+            rank_type = QuoteMessage.RankType.SPECIAL
+
+        if not header:
+            if profile["role"] == "owner":
+                header = "群主"
+            elif profile["role"] == "admin":
+                header = "管理员"
+            else:
+                header = "成员"
+
+        quote_message = QuoteMessage(
+            nick_name=profile["card"] if profile["card"] else profile["nickname"],
+            rank=int(profile['level']),
+            header=header,
+            user_id=message['user_id'],
+            message=Message([
+                MessageSegment(msg["type"], msg["data"]) for msg in message["message"]
+            ]),
+            rank_type=rank_type,
+            group_id=event.group_id,
+        )
+        
+        quote_messages.append(quote_message)
+
+    quote_image = await rend_quote_messages(quote_messages, bot)
+    image_data = io.BytesIO()
+    quote_image.save(image_data, format="PNG")
+    image_data_bytes = image_data.getvalue()
+
+    encoded_image = base64.b64encode(image_data_bytes).decode("utf-8")
+
+    await qta.send(Message(MessageSegment.image(f"base64://{encoded_image}")))
+    
+    # QuoteA Process
+    group_data_path = DATA_PATH / "random_record" / str(event.group_id)
+    if not group_data_path.exists():
+        logger.success(f"PATH {group_data_path} not exist. Creating...")
+        group_data_path.mkdir(parents=True, exist_ok=True)
+    
+    filename = f"{uuid.uuid4()}.png"
+    file_path = group_data_path / filename
+    with open(file_path, "wb") as f:
+        f.write(image_data_bytes)
+    await qta.finish("已收录喵。")
